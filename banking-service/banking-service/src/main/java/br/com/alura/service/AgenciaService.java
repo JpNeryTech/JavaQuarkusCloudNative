1851f6fd8a553c5420626cdeaacc36d0482430ae
João Pedro Nery <jppereiranery@gmail.com>
2026-05-08 21:35:41 -0300

    Criando metricas com prometheus

--- banking-service/banking-service/src/main/java/br/com/alura/service/AgenciaService.java
+++ banking-service/banking-service/src/main/java/br/com/alura/service/AgenciaService.java
@@ -6,12 +6,11 @@ import br.com.alura.domain.http.AgenciaHttp;
 import br.com.alura.domain.http.SituacaoCadastral;
 import br.com.alura.repository.AgenciaRepository;
 import br.com.alura.service.http.SituacaoCadastralHttpService;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-
-import java.util.List;
 
 @ApplicationScoped
 public class AgenciaService {
@@ -20,8 +19,10 @@ public class AgenciaService {
     private SituacaoCadastralHttpService situacaoCadastralHttpService;
 
     private final AgenciaRepository agenciaRepository;
-    AgenciaService(AgenciaRepository agenciaRepository) {
+    private final MeterRegistry meterRegistry;
+    AgenciaService(AgenciaRepository agenciaRepository, MeterRegistry meterRegistry) {
         this.agenciaRepository = agenciaRepository;
+        this.meterRegistry = meterRegistry;
     }
 
     public void cadastrar(Agencia agencia){
@@ -30,9 +31,11 @@ public class AgenciaService {
         if(agenciaHttp != null &&
                 agenciaHttp.getSituacaoCadastral().equals(SituacaoCadastral.ATIVO)){
             Log.info("A agencia com o CNPJ" +agencia.getCnpj()+ " foi cadastrada");
+            meterRegistry.counter("agencia_adicionada_counter").increment();
             agenciaRepository.persist(agencia);
         } else {
             Log.info("A agencia com o CNPJ" +agencia.getCnpj()+ " não foi cadastrada");
+            meterRegistry.counter("agencia_nao_adicionada_counter").increment();
             throw new AgenciaNaoAtivaOuNaoEncontradaException();
         }
     }
